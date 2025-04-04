2e4fd3d109e2da9ae7f856e6a108843032b7d2b6
ZMegX <picardomegan@gmail.com>
2025-04-04 19:14:27 +0100

    dictionary created needs to add render template fct

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ items_in_stock = {
         "amount": 10,
         "image_url": "https://example.com/images/nightlife.jpg"
     }
+
 }
 @app.route('/products/')
 def products():
@@ -33,12 +34,12 @@ def products():
     for sku, item in items_in_stock.items():
         if search_term.lower() in item["description"].lower():
             filtered_items.append((sku, item))
-    return render_template("products.html", products=filtered_items)
+    return render_template("product.html", products=filtered_items)
 
 
 @app.route('/tours/<sku>')
 def item(sku):
-    return render_template("tours.html", product=items_in_stock[sku], sku=sku)
+    return render_template("tours.html", items_in_stock=items_in_stock)
 
 
 @app.route('/order/<sku>', methods=["POST"])
@@ -50,53 +51,12 @@ def order(sku):
     else:
         return render_template("item_not_in_stock.html")
 
-@app.route('/admin/tours/<sku>', methods=["GET"])
-def edit_item(sku):
-    print(items_in_stock)
-    
-    if sku == items_in_stock.keys():
-        product=items_in_stock[sku]
-    else:
-        product=None
-
-    print("Edit product details:", product)
-
-    return render_template("edit_item.html", product=product, sku=sku)
-
-@app.route('/admin/tours/<sku>', methods=["POST"])
-def set_item(sku):
-    existing_item = items_in_stock.get(sku)
-    if existing_item:
-        name = request.form.get("name", existing_item["name"])
-        description = request.form.get("description", existing_item["description"])
-        price = float(request.form.get("price", existing_item["price"]))
-        amount = int(request.form.get("amount", existing_item["amount"]))
-        image_url = request.form.get("image_url", existing_item["image_url"])
-    else:
-        name = request.form["name"]
-        description = request.form["description"]
-        #price = float(request.form["price"])
-        #amount = int(request.form["amount"])
-        #image_url = request.form["image_url"]
-        price = 10
-        amount = 5
-        image_url = "https://ae01.alicdn.com/kf/HTB1dbleX_zGK1JjSspbq6zHpFXaE/Men-Shirt-2017-Spring-New-Brand-Business-Men-s-Slim-Fit-Dress-Shirt-Male-Long-Sleeves.jpg"
-
-
-    items_in_stock[sku] = {
-        "name": name,
-        "description": description,
-        "price": price,
-        "amount": amount,
-        "image_url": image_url,
-    }
-    return redirect(url_for("tours"))
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/tours")
+@app.route("/tours/")
 def tours():
     return render_template("tours.html")
 
