2fad81b545609ce2b5d226484abdfae8df9eed20
ZMegX <picardomegan@gmail.com>
2025-04-05 20:06:57 +0100

    error render template added

--- app.py
+++ app.py
@@ -40,3 +40,10 @@ def index():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.errorhandler(404) 
+def not_found_error(error):
+    return render_template('errors/404.html', error=error), 404
+
+def internal_error(error):
+    return render_template('errors/500.html', error=error), 500
